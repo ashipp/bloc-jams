a209073c9148e097b8b85c07f43c9d3913c67418
Amy Shipp <AmyShipp@Amys-MacBook-Pro.local>
2017-03-14 15:09:25 -0400

    points error fixed

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,8 +1,8 @@
         var revealPoint = function() {
             for (var i=0; i < point.length; i++) {
-                points[i].style.opacity = 1;
-                points[i].style.transform = "scaleX(1) translateY(0)";
-                points[i].style.msTransform = "scaleX(1) translateY(0)";
-                points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+                point[i].style.opacity = 1;
+                point[i].style.transform = "scaleX(1) translateY(0)";
+                point[i].style.msTransform = "scaleX(1) translateY(0)";
+                point[i].style.WebkitTransform = "scaleX(1) translateY(0)";
             }
             };
