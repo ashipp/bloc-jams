f2524fc14183c63e78fc196147e87ba5fbbd3717
Amy Shipp <AmyShipp@Amys-MacBook-Pro.local>
2017-03-25 12:25:27 -0400

    checkpoint-9-assignment

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,12 +1,16 @@
-var pointsArray = document.getElementsbyClassName('point');
+var pointsArray = document.getElementsByClassName('point');
 
 var animatePoints = function(points) {
     var revealPoint= function(index) {
         points[index].style.opacity = 1;
-        points[index].syle.transform = "scaleX(1) translateY(0)";
-        points[index].style.msTransform = "scaleX(1) translateY(0)";
-        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
+        points[index].syle.transform= "scaleX(1) translateY(0)";
+        points[index].style.msTransform= "scaleX(1) translateY(0)";
+        points[index].style.WebkitTransform= "scaleX(1) translateY(0)";
     }
+    for (var i = 0; i < points.length; i++) {
+        revealPoint(i);
+    }
+
 }
 
 window.onload = function() {
@@ -15,6 +19,7 @@ window.onload = function() {
         animatePoints(pointsArray);
     }
     var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
     
     window.addEventListener('scroll', function(event) {
         if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
